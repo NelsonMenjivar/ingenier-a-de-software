9b17996250fb85ea72957a5d0c63d2d6bda3c59f
Cristian Sanchez <sancas19@gmail.com>
2016-08-25 23:32:10 -0600

    Arreglado el carousel
    Se agrego el carousel que tenia algunos problemas con responsive

--- Parallax/js/main.js
+++ Parallax/js/main.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
-    if($("html").hasClass("mobile")){
-        $("#content3").attr("data-stellar-background-ratio", "0.5");
-    }
-    $.stellar({
-        horizontalScrolling: false,
-        responsive: true
-    });
+	if($("html").hasClass("mobile")){
+		$("#content3").attr("data-stellar-background-ratio", "0.5");
+		}
+	$.stellar({
+		horizontalScrolling: false,
+		responsive: true
+	})
+
+	$('a[href^="#"]').bind('click.smoothscroll',function (e) {
+
+		e.preventDefault();
+
+		var target = this.hash,
+		$target = $(target);
+
+		$('html, body').stop().animate( {
+		'scrollTop': $target.offset().top
+		}, "slow", 'swing', function () {
+		window.location.hash = target;
+		});
+	})
+
+	$('li > a').click(function() {
+	    $('li').removeClass();
+	    $(this).parent().addClass('active');
+	});
 });
